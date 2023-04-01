3f626e4f266469a9884ade2ddf98281acd3e1e8c
Cole Van Vlack <cvanvlack@gmail.com>
2023-04-01 12:51:09 -0400

    Removed duplicate print fixed bug on final print statement

--- bin/virtek_vision.dart
+++ bin/virtek_vision.dart
@@ -1,18 +1,16 @@
 import 'package:virtek_vision/building.dart';
 import 'package:virtek_vision/location.dart';
 import 'package:virtek_vision/locations_checked.dart';
-import 'package:virtek_vision/print_search.dart';
 import 'package:virtek_vision/search_tree.dart';
 
 void main(List<String> arguments) {
   final building = Building.problem();
   final startingLocation = Location(3, 2);
   final locationsChecked = LocationsChecked(building);
-  printSearch(building, locationsChecked);
   final foundLocation = search(building, locationsChecked, startingLocation);
   if (foundLocation == null) {
     print('No Intruder, all safe');
   } else {
-    print('Found Intruder at X: ${foundLocation.x},Y: ${foundLocation.x}');
+    print('Found Intruder at X: ${foundLocation.x},Y: ${foundLocation.y}');
   }
 }
